8934a92c43bf302172efb9ea7dc3a9355c6f386f
Jonny191994 <31445472+Jonny191994@users.noreply.github.com>
2020-02-13 18:32:54 -0500

    Added project folder
    Class folder added for first time

---
+++ Assets/Scripts/W2Board.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2Board : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 PlayerOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerOrigin = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.transform.position += new Vector3(1, 0, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.transform.position += new Vector3(-1, 0, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.transform.position += new Vector3(0, 0, -1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            player.transform.position += new Vector3(0, 0, 1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            player.transform.position += new Vector3(0, 1, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            player.transform.position += new Vector3(0, -1, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            player.transform.position = PlayerOrigin;
+
+        }
+    }
+}
