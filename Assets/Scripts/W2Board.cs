add6c872c3fd6400aa4e433cfd091082327172a0
Jonny191994 <31445472+Jonny191994@users.noreply.github.com>
2020-02-13 20:16:15 -0500

    end of class, array collisions working

--- Assets/Scripts/W2Board.cs
+++ Assets/Scripts/W2Board.cs
@@ -6,11 +6,20 @@ public class W2Board : MonoBehaviour
 {
     public GameObject player;
     private Vector3 PlayerOrigin;
+    private Vector3[] HazardOrigin;
+    public Transform[] hazard;
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerOrigin = player.transform.position;
+
+
+
+        for (int i = 0; i < hazard.Length; i++)
+        {
+            HazardOrigin[i] = hazard[i].transform.position;
+        }
     }
 
     // Update is called once per frame
@@ -19,43 +28,48 @@ public class W2Board : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             player.transform.position += new Vector3(1, 0, 0);
-
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
             player.transform.position += new Vector3(-1, 0, 0);
-
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
             player.transform.position += new Vector3(0, 0, -1);
-
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
             player.transform.position += new Vector3(0, 0, 1);
-
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             player.transform.position += new Vector3(0, 1, 0);
-
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
             player.transform.position += new Vector3(0, -1, 0);
-
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             player.transform.position = PlayerOrigin;
+            //hazard.position = HazardOrigin;
+            
+        }
 
+        for (int i = 0; i < hazard.Length; i++)
+        {
+            if (hazard[i].position == player.transform.position)
+            {
+                player.transform.position = PlayerOrigin;
+                hazard[i].position = HazardOrigin[i];
+
+            }
         }
     }
 }
