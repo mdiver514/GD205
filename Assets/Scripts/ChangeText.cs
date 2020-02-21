c3100158bdc481be8247338f27f591d2a4e85bc0
Jonny191994 <31445472+Jonny191994@users.noreply.github.com>
2020-02-20 20:05:40 -0500

    Week4 end of class
    UI stuff

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text playerStatus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerStatus.text = "You Died";
+    }
+
+}
--- Assets/Scripts/W2Board.cs
+++ Assets/Scripts/W2Board.cs
@@ -8,12 +8,18 @@ public class W2Board : MonoBehaviour
     private Vector3 PlayerOrigin;
     private Vector3[] HazardOrigin;
     public Transform[] hazard;
+    AudioSource audPlayer;
+    public AudioClip moveClip, hazardClip;
+    public GameObject posTile;
+    public GameObject posTile2;
+    public GameObject posTile3;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerOrigin = player.transform.position;
-
+        audPlayer = GetComponent<AudioSource>();
 
 
         for (int i = 0; i < hazard.Length; i++)
@@ -28,38 +34,45 @@ public class W2Board : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             player.transform.position += new Vector3(1, 0, 0);
+            audPlayer.PlayOneShot(moveClip);
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
             player.transform.position += new Vector3(-1, 0, 0);
+            audPlayer.PlayOneShot(moveClip);
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
             player.transform.position += new Vector3(0, 0, -1);
+            audPlayer.PlayOneShot(moveClip);
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
             player.transform.position += new Vector3(0, 0, 1);
+            audPlayer.PlayOneShot(moveClip);
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             player.transform.position += new Vector3(0, 1, 0);
+            audPlayer.PlayOneShot(moveClip);
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
             player.transform.position += new Vector3(0, -1, 0);
+            audPlayer.PlayOneShot(moveClip);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             player.transform.position = PlayerOrigin;
+            audPlayer.PlayOneShot(hazardClip);
             //hazard.position = HazardOrigin;
-            
+
         }
 
         for (int i = 0; i < hazard.Length; i++)
@@ -67,9 +80,25 @@ public class W2Board : MonoBehaviour
             if (hazard[i].position == player.transform.position)
             {
                 player.transform.position = PlayerOrigin;
-                hazard[i].position = HazardOrigin[i];
-
+                audPlayer.PlayOneShot(hazardClip);
+                
             }
+        }
+
+        if (posTile.transform.position + new Vector3(0, 1, 0) == player.transform.position)
+        {
+            posTile.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (posTile2.transform.position + new Vector3(0, 1, 0) == player.transform.position)
+        {
+            posTile2.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (posTile3.transform.position + new Vector3(0, 1, 0) == player.transform.position)
+        {
+            posTile3.GetComponent<Renderer>().material.color = Color.red;
         }
     }
 }
+
